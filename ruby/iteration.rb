93009d26f65f3ef3493f41ccc60cb57b9357086a
Danny R <wegotopancakeshouse@gmail.com>
2016-09-29 02:59:14 -0700

    updated iteration.rb

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,53 @@
+## Writing a method that takes a block. ##
+
+def poster_collection
+	puts "Banksy, Obey, Mr. Brainwash"
+	yield ("Stout", "Moss")
+end
+
+poster_collection{|artist1, artist2| puts "Also #{name1} and #{name2}" }
+
+
+## Release 1. ##
+
+
+## Here is an array of 3 virtual drawers of a flat file. ##
+#flat_file = [drawer_1, drawer_2, drawer_3]
+
+
+## Here is a virtual hash of 3 prints. ##
+screen_prints = {
+	banksy: "Pulp Fiction",
+	obey: "They Live",
+	mbw:  "Going To NY"
+
+}
+
+
+## This code will iterate through the array of drawers. ##
+#flat_file.each do |art|
+#	puts art
+#end
+
+
+## This code will iterate through the hash of prints. ##
+#screen_prints.each do |artist, printname|
+#end
+
+
+## This code will iterate using the .map! function on an array. ##
+#puts "Original Information"
+#p flat_file
+#flat_file.map! do |drawers|
+#	 drawers.upcase
+#end
+
+#puts "Modified Information"
+#p flat_file
+
+
+## End of Release 1. ##
+
 mj_plants = {
 	'OG Kush' => 'veg',
 	'Pineapple Express' => 'flower',
@@ -79,6 +129,8 @@ mj_plants = mj_plants.drop_while {|plant_name, status| plant_name.length > 9}
 p mj_plants
 
 ## Tried a few things, can't get this proper. ##
+
+
 
 
 
