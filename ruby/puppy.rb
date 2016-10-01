ca650b484ef63e3a8d66f3ccf622234ddb9152bc
Danny R <wegotopancakeshouse@gmail.com>
2016-10-01 11:38:01 -0700

    Added Puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# SPECIES ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# Legs: 4
+# Ears: Floppy
+# Eye count: 2
+# Name: varies
+# Color: varies
+# 
+# BEHAVIOR -----------------------
+# Energetic
+# Playful
+# Happy
