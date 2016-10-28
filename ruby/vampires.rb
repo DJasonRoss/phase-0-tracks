d21c8361452a0dd6578fb50408f92ed07797ea53
DJasonRoss <wegotopancakeshouse@gmail.com>
2016-10-27 21:55:24 -0700

    Update vampires.rb

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,70 +1,92 @@
+# Vampires v2
+
+
+# data collection / storing in variables.
+
+
+
 puts "How many employees will be processed?"
-employee_number = gets.chomp.to_i
-counter = 0
+to_process = gets.chomp.to_i
+until to_process == 0 
 
-until counter == employee_number
+puts "What is your name?"
+employ_name = gets.chomp.downcase!
 
-	puts "What is your name?"
-	name = gets.chomp
 
-	puts "How old are you?"
-	age = gets.chomp.to_i
 
-	puts "In what year were you born?"
-	birth_year = gets.chomp.to_i
+name_test = true
+if employ_name == "drake cula" || employ_name == "tu fang" || employ_name == "dr. acula"
+	name_test = false
 
-	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-	garlic_bread = gets.chomp
+end
 
-	puts "Would you like to enroll in the company’s health insurance?"
-	health_ins = gets.chomp
 
-	if (age + birth_year < 2015 || age + birth_year > 2016)
+puts "How old are you?"
+employ_age = gets.chomp.to_i
+
+puts "In which year were you born?"
+employ_birth_year = gets.chomp.to_i
+
+puts "Would you like garlic bread?"
+garlic_bread = gets.chomp.downcase
+
+puts "Health insurance?"
+insurance = gets.chomp
+
+#Detetction logic.
+
+if garlic_bread  == ("yes" || "y")
+	 bread_status = true
+	else bread_status = false
+end
+
+if insurance  == ("yes" || "y")
+	 insurance_status = true
+	else insurance_status = false
+
+end
+
+
+if (employ_age + employ_birth_year < 2015 || employ_age + employ_birth_year > 2016)
 		age_test = false
 	else
 		age_test = true
-	end
-
-	if name = "Drake Cula" || name = "Tu Fang" || name = "Dr. Acula"
-		vamp_test = true
-	else
-		vamp_test = false
-	end
-
-	if garlic_bread == "yes" || health_ins == "yes"
-		if age_test == true
-			vampire_status = "Probably not a vampire"
-		else
-			vampire_status = "Probably a vampire"
-		end
-
-	elsif age_test == false && garlic_bread == "no" && health_ins == "no"
-		vampire_status = "Almost certainly a vampire" 
-	elsif vamp_test == true
-		vampire_status = "Definitely a vampire"
-	else 
-		vampire_status = "Results inconclusive." 
-	end
-
-	puts "Please type any allergies you have and type \"done\" when complete"
-		
-		allergy = "none"
-		while allergy != "done"
-		allergy = gets.chomp
-			
-			if allergy == "sunshine"
-					vampire_status = "Probably a vampire"
-					allergy = "done"
-			end
-		end
-		
-	puts vampire_status
-	puts ""
-	counter += 1
-
-	if counter < employee_number
-		puts "Employee ##{counter + 1}"
-	else
-		puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends!!!"
-	end
 end
+
+
+if name_test == true && age_test == true && (bread_status == true || insurance_status == true)
+	puts "Probably not a vampire."
+	elsif name_test == false && age_test == true && (bread_status == true || insurance_status == true)
+	puts "Totes a vampire."
+end
+
+
+if name_test == true && age_test == false && (bread_status == true || insurance_status == true)
+		puts "Probably a vampire."
+	elsif name_test == false && age_test == false && (bread_status == true || insurance_status == true)
+	puts "Totes a vampire."
+end
+
+if age_test = true && (bread_status == false && insurance_status == false)
+			puts "Almost certainly a vampire."
+		elsif age_test = true && (bread_status == false && insurance_status == false)
+		puts "Totes a vampire."
+end
+# # Adjusts counter 
+to_process -= 1
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
