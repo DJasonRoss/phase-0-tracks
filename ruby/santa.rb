d1a5c7591c6485793b26fad4bb87a4d032413166
DJasonRoss <wegotopancakeshouse@gmail.com>
2016-11-10 18:03:33 -0800

    updated santas.rb

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,33 +1,28 @@
-class Santa
-
-	attr_reader :enthicity, :age
-
-	def speak
-		puts "Ho, Ho Ho!  Happy Holidays!!!"
-	end
-
-	def eat_milk_and_cookies(cookie_type)
-		puts "That was a good #{cookie_type} cookie!!"
-	end
-
-	def initialize(gender,enthicity)
-		puts "Initializing Santa Instance..."
+class Santa 
+	attr_accessor :enthnicity, :age
+	
+	def initialize(gender, enthnicity)
+		puts "Initializing Santa..."
 		@gender = gender
-			puts "#{@gender}"
-		@enthicity = enthicity
-			puts "#{@enthicity}"
+		@enthnicity = enthnicity
 		@age = rand(140)
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-
+	end
+	
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
 
-	def celebrate_birthday
-			@age += 1
-				if @age == 1
-			puts "Happy Birthday, Santa!! You are now #{@age} year old."
-				else puts "Happy Birthday, Santa!! You are now #{@age} years old."
-		end
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}!"
 	end
+
+	def birthday
+		age = @age += 1
+		puts "This Santa is now #{@age} years old!"
+	end
+	
+
 	
 	def get_mad_at(reindeer_name)
 			puts "I'm MAD AT #{reindeer_name}"
@@ -39,67 +34,46 @@ class Santa
 	def gender(new_gender)
 			@gender = new_gender
 		end
-	
+
 end
-			
-# claus = Santa.new("male","russian","169",9)
-# claus.speak
-# claus.eat_milk_and_cookies("Oatmeal")
-# claus.enthicity
-# # claus.age
-# claus.celebrate_birthday
-# claus.celebrate_birthday
-# claus.celebrate_birthday
-# claus.age
-# claus.get_mad_at("Rudolph")
+
+################################################################################################
+
+# nick = Santa.new("Male", "Black")
+# p nick.age
+# p nick.enthnicity
+# p nick.gender("female")
 
 
-santas = []
+
+
+
+# santas = []
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 
 
-100.times do
+
+
+25.times do
 	claus = Santa.new(example_genders.sample, example_ethnicities.sample)
-    puts "This Santa is #{claus.age} years old."
+	p claus
+    
 end
 
 
-
-
-#claus = Santa.new(example_genders.sample, example_ethnicities.sample)
-#Above driver code works.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+################################################################################################
+# claus.age
+# claus.speak
+# claus.eat_milk_and_cookies("oatmeal")
+# Release zero complete
+# Start realease one.
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+# p santas
