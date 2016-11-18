81b834ed8e4aabbe4bab15f3b722ecc754f5c33f
DJasonRoss <wegotopancakeshouse@gmail.com>
2016-11-18 15:11:27 -0800

    created explore.js

---
+++ js/explore.js
@@ -0,0 +1,25 @@
+// need to make a method to reverse a sting.
+// so that reverse(hello) returns "olleh"
+
+// this function called reverse takes a word
+// param, splits the word into an array, reverses the
+// array and then joins the array back into the reversed
+// word.
+
+// I have no idea how to 
+
+
+var word_holder = "";
+
+
+
+function reverse(word) {
+  if (word_holder != word.split('').reverse().join(''))
+  console.log("THAT IS NOT THE SECRET WORD");
+  	return word.split('').reverse().join('');
+	
+}
+
+
+
+reverse("hello")
